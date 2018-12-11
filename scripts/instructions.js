ef32626d1e262ebba10dea51a03a13bd33cb0fcb
josephhess <readysteadyco@gmail.com>
2018-12-10 16:51:09 -0800

    remove close instructions button, make instruction button open and close the section, initial styling: more to do

--- scripts/instructions.js
+++ scripts/instructions.js
@@ -1,12 +1,7 @@
 function showHideInstructions(){
   $('#how-to-play').on("click", e => {
     e.preventDefault();
-    $('#player-instructions ul, #close-instructions').removeClass('sr-only');
-  })
-
-  $('#close-instructions').on("click", e => {
-    e.preventDefault();
-    $('#player-instructions ul, #close-instructions').addClass('sr-only');
+    $('#player-instructions ul, #close-instructions').toggleClass('sr-only');
   })
 }
 
